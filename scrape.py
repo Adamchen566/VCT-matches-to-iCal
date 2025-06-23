127cc09afe928e0ebb4811bea42b720294cd8d29
Adamchen566 <jiashaoc@student.unimelb.edu.au>
2025-06-23 12:35:45 +1000

    多伦多结束

--- scrape.py
+++ scrape.py
@@ -307,29 +307,29 @@ Toronto_event = []
 # get_match_info(url_2025_stage1_emea, stage1_event, 'EMEA', 0)
 # get_match_info(url_China_Evolution_Act2, evo2_event, 'CN-EVO', 0)
 # get_match_info(url_Asian_Champions_League, ACL_event, 'Asian-ACL', 0)
-get_match_info(url_ewc, ewc_event, 'EWC', 0)
-get_match_info(url_master_Toronto, Toronto_event, 'Toronto', 1)
-sorted_event = sorted(ewc_event + Toronto_event, key=sort_key)
+# get_match_info(url_ewc, ewc_event, 'EWC', 0)
+get_match_info(url_master_Toronto, Toronto_event, 'Toronto', 0)
+# sorted_event = sorted(ewc_event + Toronto_event, key=sort_key)
 
 # Stage2 + Paris
 # get_match_info(url_champion_Paris, Paris_event, 'Paris', 0)
 
 
 # OnGoing Events
-OnGoing_event = sorted_event
-matchprint(OnGoing_event)
-file_name = '/vct_OnGoing.txt'
-title = 'vct_OnGoing'
-save2file(OnGoing_event, file_path, file_name, title)
-name = 'vct_OnGoing'
-create_ics_file(url_vlr, OnGoing_event, name)
+# OnGoing_event = sorted_event
+# matchprint(OnGoing_event)
+# file_name = '/vct_OnGoing.txt'
+# title = 'vct_OnGoing'
+# save2file(OnGoing_event, file_path, file_name, title)
+# name = 'vct_OnGoing'
+# create_ics_file(url_vlr, OnGoing_event, name)
 
 
 # Add finished envets to completed ics file
 ics_file_path = 'D:\\BackUp\\self-work\\VCT-matches-to-iCal\\Calendar_Files\\vct_completed.ics'
 # update_ics_file(url_2025_stage1_cn, stage1_event, ics_file_path)
 # update_ics_file(url_China_Evolution_Act2, evo2_event, ics_file_path)
-# update_ics_file(url_Asian_Champions_League, ACL_event, ics_file_path)
+update_ics_file(url_master_Toronto, Toronto_event, ics_file_path)
 
 # 一次生成所有的文件
 if __name__ == "__main__":
