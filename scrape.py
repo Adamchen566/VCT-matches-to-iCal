df0aeb46ebad90e3b1e66a964f65d901bc7f1398
Adamchen566 <jiashaoc@student.unimelb.edu.au>
2025-09-01 19:06:08 +1000

    stage2完结

--- scrape.py
+++ scrape.py
@@ -317,8 +317,8 @@ get_match_info(url_2025_stage2_amer, state2_event, 'AMER', 0)
 get_match_info(url_2025_stage2_pac, state2_event, 'PAC', 0)
 get_match_info(url_2025_stage2_emea, state2_event, 'EMEA', 0)
 get_match_info(url_champion_Paris, Paris_event, 'Paris', 0)
-get_match_info(url_China_Evolution_Act3, evo3_event, 'CN-EVO', 0)
-sorted_event = sorted(state2_event + evo3_event + Paris_event, key=sort_key)
+# get_match_info(url_China_Evolution_Act3, evo3_event, 'CN-EVO', 0)
+sorted_event = sorted(state2_event, key=sort_key)
 
 # OnGoing Events
 OnGoing_event = sorted_event
@@ -332,10 +332,4 @@ create_ics_file(url_vlr, OnGoing_event, name)
 
 # Add finished envets to completed ics file
 ics_file_path = 'D:\\BackUp\\self-work\\VCT-matches-to-iCal\\Calendar_Files\\vct_completed.ics'
-# update_ics_file(url_2025_stage2_cn, state2_event, ics_file_path)
-
-# 一次生成所有的文件
-if __name__ == "__main__":
-    # generate_all_ics()
-    # generate_all_txt()
-    print("init function")
+update_ics_file(url_2025_stage2_cn, state2_event, ics_file_path)
